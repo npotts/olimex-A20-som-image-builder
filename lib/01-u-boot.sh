2a31ef6b32b9bd7788fe7b21fb40cf8c9deaf82a
npotts <npotts@users.noreply.github.com>
2015-04-01 10:52:01 -0600

    Updated u-boot build process

--- lib/01-u-boot.sh
+++ lib/01-u-boot.sh
@@ -3,19 +3,28 @@
 # it will build for A20-OLinuXino-Lime2_defconfig
 build_uboot() { 
 	#builds uboot from sources Submodule of repo is https://github.com/RobertCNelson/u-boot
-	pushd u-boot
-	echo -n "Checking for u-boot ..."
-	[ -f ../$OUTPUT_DIR/u-boot_4.0.0-rc4.tgz ] && echo " already exists" && popd && return;
-	echo "no u-boot.  Proceeding with building"
+	pushd u-boot &> /dev/null
+	echo -n "*********************** Checking if we need to build u-boot: "
+	[ -f ../$OUTPUT_DIR/u-boot_4.0.0-rc4.tgz ] && echo " nope" && popd && return;
+	echo "yep."
 	
+	echo "*********************** uboot: Cleaning"
 	make -s  CROSS_COMPILE=arm-linux-gnueabihf- clean
 	if [ -z $1 ]; then
-		make -j5 CROSS_COMPILE=arm-linux-gnueabihf- $1
+		echo "*********************** uboot: using default Lime2 config"
+		make -j5 CROSS_COMPILE=arm-linux-gnueabihf- A20-OLinuXino-Lime2_defconfig &> ../$OUTPUT_DIR/u-boot.log
 	else
-		make -j5 CROSS_COMPILE=arm-linux-gnueabihf- A20-OLinuXino-Lime2_defconfig
+		echo "*********************** uboot: Provided config."
+		make -j5 CROSS_COMPILE=arm-linux-gnueabihf- $1 &> ../$OUTPUT_DIR/u-boot.log
 	fi
-	make -j5 CROSS_COMPILE=arm-linux-gnueabihf- #build u-boot
+	[ ! $? -eq 0 ] && echo "FAILED!!!!!!" && exit 
+	echo "*********************** uboot: Building"
+	make -j5 CROSS_COMPILE=arm-linux-gnueabihf-  &>> ../$OUTPUT_DIR/u-boot.log #build u-boot
+	[ ! $? -eq 0 ] && echo "FAILED!!!!!!" && exit
+	echo "*********************** uboot: Packaging"
 	#package it
-	tar cPfz ../$OUTPUT_DIR/u-boot_4.0.0-rc4.tgz u-boot-sunxi-with-spl.bin
-	popd
+	tar cPfz ../$OUTPUT_DIR/u-boot_4.0.0-rc4.tgz u-boot-sunxi-with-spl.bin &>> ../$OUTPUT_DIR/u-boot.log
+	[ ! $? -eq 0 ] && echo "FAILED!!!!!!" && exit
+	echo "*********************** uboot: Done"
+	popd &> /dev/null
 }
